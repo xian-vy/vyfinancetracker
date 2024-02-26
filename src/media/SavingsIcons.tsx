6fddb38caf920d9cadc63b46f39819ab0b1fc67a
xianvy <xianvy0000@gmail.com>
2024-02-26 10:48:55 +0800

    renamed Media to media

---
+++ src/media/SavingsIcons.tsx
@@ -0,0 +1,44 @@
+import DirectionsCarFilledOutlinedIcon from "@mui/icons-material/DirectionsCarFilledOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import LiveHelpOutlinedIcon from "@mui/icons-material/LiveHelpOutlined";
+import LocalAtmOutlinedIcon from "@mui/icons-material/LocalAtmOutlined";
+import SportsFootballIcon from "@mui/icons-material/SportsFootball";
+import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
+import VideogameAssetOutlinedIcon from "@mui/icons-material/VideogameAssetOutlined";
+import CelebrationOutlinedIcon from "@mui/icons-material/CelebrationOutlined";
+import DevicesOtherOutlinedIcon from "@mui/icons-material/DevicesOtherOutlined";
+import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
+import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
+import SavingsOutlinedIcon from "@mui/icons-material/SavingsOutlined";
+import FamilyRestroomOutlinedIcon from "@mui/icons-material/FamilyRestroomOutlined";
+import ChildFriendlyOutlinedIcon from "@mui/icons-material/ChildFriendlyOutlined";
+import FlightOutlinedIcon from "@mui/icons-material/FlightOutlined";
+import CakeOutlinedIcon from "@mui/icons-material/CakeOutlined";
+import PianoOutlinedIcon from "@mui/icons-material/PianoOutlined";
+import PetsOutlinedIcon from "@mui/icons-material/PetsOutlined";
+import DoNotDisturbAltIcon from "@mui/icons-material/DoNotDisturbAlt";
+
+import React from "react";
+const SavingsIcons = [
+  { name: "Gaming", icon: <VideogameAssetOutlinedIcon /> },
+  { name: "House", icon: <HomeOutlinedIcon /> },
+  { name: "Money", icon: <AttachMoneyOutlinedIcon /> },
+  { name: "Gadget", icon: <DevicesOtherOutlinedIcon /> },
+  { name: "Savings", icon: <SavingsOutlinedIcon /> },
+  { name: "Travel", icon: <FlightOutlinedIcon /> },
+  { name: "Baby", icon: <ChildFriendlyOutlinedIcon /> },
+  { name: "Family", icon: <FamilyRestroomOutlinedIcon /> },
+  { name: "Car", icon: <DirectionsCarFilledOutlinedIcon /> },
+  { name: "Pet", icon: <PetsOutlinedIcon /> },
+  { name: "Music", icon: <PianoOutlinedIcon /> },
+  { name: "Dollar", icon: <LocalAtmOutlinedIcon /> },
+  { name: "Education", icon: <SchoolOutlinedIcon /> },
+  { name: "Sports", icon: <SportsFootballIcon /> },
+  { name: "Motorbike", icon: <TwoWheelerIcon /> },
+  { name: "Cake", icon: <CakeOutlinedIcon /> },
+  { name: "No Icon", icon: <DoNotDisturbAltIcon /> },
+  { name: "Celebration", icon: <CelebrationOutlinedIcon /> },
+  { name: "Uncategorized", icon: <LiveHelpOutlinedIcon /> },
+];
+
+export default SavingsIcons;
