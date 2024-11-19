05ea74a09abeea43aa1857d71f371eb72e5793fb
jovie00 <xianvy0000@gmail.com>
2024-11-19 17:27:36 +0800

    Create helper for debt total sum calculator

---
+++ src/helper/DebtHelper.ts
@@ -0,0 +1,45 @@
+import { DEBT_STATUS } from "../constants/constants";
+import DebtModel from "../models/DebtModel";
+
+
+
+export const calculateDebtSumByDate = (data: DebtModel[], startDateProp: Date, endDateProp: Date)  => {
+    const filteredData = data
+  
+    //plus to balance
+    const borrowedNotPaid = filteredData.reduce((total, item) => {
+      if (item.isCreditor === false && item.status === DEBT_STATUS.InProgress) {
+        return total + item.amount;
+      }
+      return total;
+    }, 0);
+  
+    //minus to balance
+    const borrowedPaid = filteredData.reduce((total, item) => {
+      if (item.isCreditor === false && item.status === DEBT_STATUS.Complete) {
+        return total + item.amount;
+      }
+      return total;
+    }, 0);
+  
+       //plus to balance
+    const lendedPaid = filteredData.reduce((total, item) => {
+      if (item.isCreditor === true && item.status === DEBT_STATUS.Complete) {
+        return total + item.amount;
+      }
+      return total;
+    }, 0);
+  
+    //minus to balance
+    const lendedNotPaid = filteredData.reduce((total, item) => {
+      if (item.isCreditor === true && item.status === DEBT_STATUS.InProgress) {
+        return total + item.amount;
+      }
+      return total;
+    }, 0);
+
+    const total = (lendedPaid + borrowedNotPaid) - (lendedNotPaid + borrowedPaid);
+
+
+    return  {total,borrowedPaid, borrowedNotPaid, lendedPaid, lendedNotPaid};
+  }
