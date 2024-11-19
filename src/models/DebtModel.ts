8912e6672d5c02b73463b12348a575b9085669bf
jovie00 <xianvy0000@gmail.com>
2024-11-19 16:05:53 +0800

    Update DebtModel.ts

--- src/models/DebtModel.ts
+++ src/models/DebtModel.ts
@@ -1,15 +1,18 @@
 import { Timestamp } from "firebase/firestore";
+import { DEBT_STATUS } from "../constants/constants";
 
 interface DebtModel {
   id: string;
-  name: string;
-  description: string;
+  entity : string;
+  note: string;
   amount: number;
   startDate: Timestamp;
   endDate: Timestamp;
+  isCreditor: boolean;
+  account_id : string;
   // rate: number;
   // frequency: string;
-  status: string;
+  status: DEBT_STATUS;
   lastModified?: Timestamp;
 }
 
