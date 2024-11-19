36c7a24895c3a73b72f843c3c352c0578387ff5c
jovie00 <xianvy0000@gmail.com>
2024-11-19 16:05:58 +0800

    Update debtAction.ts

--- src/redux/actions/debtAction.ts
+++ src/redux/actions/debtAction.ts
@@ -3,19 +3,19 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { addDebts, deleteDebts, getDebts, updateDebts } from "../../firebase/DebtService";
 import DebtModel from "../../models/DebtModel";
 
-export const fetchDebts= createAsyncThunk("Debts/fetchDebt", async () => {
+export const fetchDebts= createAsyncThunk("debt/fetchDebt", async () => {
   return getDebts();
 });
-export const adddebtAction = createAsyncThunk("Debts/addDebts", async (data: DebtModel) => {
+export const adddebtAction = createAsyncThunk("debt/addDebts", async (data: DebtModel) => {
   const id = await addDebts(data);
   data.id = id;
   return data;
 });
-export const updateDebtsAction = createAsyncThunk("Debts/updateDebts", async (data: DebtModel) => {
+export const updateDebtsAction = createAsyncThunk("debt/updateDebts", async (data: DebtModel) => {
   updateDebts(data);
   return data;
 });
-export const deleteDebtsAction = createAsyncThunk("Debts/deleteDebts", async (Id: string) => {
+export const deleteDebtsAction = createAsyncThunk("debt/deleteDebts", async (Id: string) => {
   deleteDebts(Id);
   return Id;
 });
