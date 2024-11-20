a321dc2d3f8e9cbd7d4b9f4c49d7f5900e5526ef
jovie00 <xianvy0000@gmail.com>
2024-11-20 09:54:27 +0800

    Add debt in in total networth calculation

--- src/components/NavMenu/TotalNetWorth.tsx
+++ src/components/NavMenu/TotalNetWorth.tsx
@@ -8,6 +8,8 @@ import { ReactComponent as Coin } from "../../media/coin.svg";
 import SavingGoalsContributionModel from "../../models/SavingGoalsContribution";
 import ExpenseModel from "../../models/ExpenseModel";
 import IncomeModel from "../../models/IncomeModel";
+import DebtModel from "../../models/DebtModel";
+import { generateDebtAmounts } from "../../helper/DebtHelper";
 
 const TotalNetWorth = ({ collapsedDrawer }: { collapsedDrawer: boolean }) => {
   const theme = useTheme();
@@ -17,19 +19,27 @@ const TotalNetWorth = ({ collapsedDrawer }: { collapsedDrawer: boolean }) => {
   const savingsContribution : SavingGoalsContributionModel[] = useSelector((state: RootState) => state.savingsContribution.contribution);
   const expenses : ExpenseModel[] = useSelector((state: RootState) => state.expenses.expenses);
   const income : IncomeModel[]= useSelector((state: RootState) => state.income.income);
+  const debt : DebtModel[]= useSelector((state: RootState) => state.debt.debt);
+  const debtItems = generateDebtAmounts(debt);
+  const netDebt =   debtItems.reduce((sum, item) => {
+    if (item.amount < 0) {
+      return sum - item.amount; // Debt Owed (Borrowed Paid, Lended UnPaid)
+    } else {
+      return sum + item.amount; // Debt Owned (Borrowed UnPaid, Lended Paid)
+    }
+  }, 0) || 0;
 
   const totalSavingsContribution = calculateTotalSum(savingsContribution, "amount");
   const totalExpenses = calculateTotalSum(expenses, "amount");
   const totalIncome = calculateTotalSum(income, "amount");
-
-  const totalBalance = totalIncome - totalExpenses - totalSavingsContribution;
+  const totalBalance = totalIncome - totalExpenses - totalSavingsContribution - netDebt;
   return (
     <div>
       {!collapsedDrawer && (
         <Tooltip title="Total NetWorth">
           <Stack direction="row" my={0.5} justifyContent="center" alignItems="center" sx={{ cursor: "pointer" }}>
             <Coin fill={isDarkMode ? "#ccc" : "#666"} style={{ width: "14px", height: "14px", marginRight: "5px" }} />
-            <Typography textAlign="center" variant="h4" sx={{ color: totalBalance < 0 ? "salmon" : "inherit" }}>
+            <Typography textAlign="center" component={"div"} variant="h4" sx={{ color: totalBalance < 0 ? "salmon" : "inherit" }}>
               {isMasked
                 ? "****"
                 : totalBalance === 0
