a368999f6d86c53d00c48d28555882788f1a8b3b
xianvy <xianvy0000@gmail.com>
2024-11-23 16:28:46 +0800

    Add debt total summary per type

---
+++ src/components/Debt/DebtSummary.tsx
@@ -0,0 +1,58 @@
+import { Grid, Paper, Typography, useTheme } from '@mui/material'
+import { FilterTimeframe } from '../../constants/timeframes'
+import { calculateDebtByType } from '../../helper/DebtHelper'
+import DebtModel from '../../models/DebtModel'
+
+interface Props {
+    debt : DebtModel[]
+}
+const DebtSummary = ({debt}: Props) => {
+    const theme = useTheme();
+    const isDarkMode = theme.palette.mode === "dark";
+    const {borrowedPaid, borrowedNotPaid, lendedPaid, lendedNotPaid} = calculateDebtByType(debt, FilterTimeframe.AllTime, undefined, undefined);
+
+    const summary = [
+        {
+            title: "Borrowed/Unpaid",
+            amount : borrowedNotPaid,
+            sign : "+",
+            color: "green"
+        },
+        {
+            title: "Lended/Paid",
+            amount : lendedPaid,
+            sign : "+",
+              color: "green"
+        },
+        {
+            title: "Borrowed/Paid",
+            amount : borrowedPaid,
+            sign : "-",
+            color: "salmon"
+        },
+        {
+            title: "Lended/Unpaid",
+            amount : lendedNotPaid,
+            sign : "-",
+              color: "salmon"
+        },
+    ]
+  return (
+     <Grid container spacing={2} justifyContent="center" alignItems="center" width="100%" mx="auto" my={2}>
+        {summary.map((item, index) => (
+            <Grid  container justifyContent="center" item xs={6} md={3} key={index}>
+                <Paper  sx={{ borderRadius: 2, p: 2, display: "flex",flexDirection: "column", justifyContent: "center", alignItems: "center" , width: "100%"}}     variant={isDarkMode ? "elevation" : "outlined"}>
+                    <Typography variant='body2' sx={{fontWeight:"bold",mb:2}}>
+                        {item.title}
+                    </Typography>
+                    <Typography sx={{color: item.color}}>
+                    {item.sign} {item.amount}
+                    </Typography>
+                </Paper>
+            </Grid>
+        ))}
+     </Grid>
+)
+}
+
+export default DebtSummary
